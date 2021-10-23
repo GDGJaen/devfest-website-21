309f20bab12e786d8380a9266795c2f321b28c6a
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-10-23 14:40:48 +0200

    Adaptando toda la página para gdg jaén (en español)

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,11 @@
-import styled from 'styled-components'
-import theme from './theme'
-import Link from 'next/link'
-import GDGLogo from './icons/gdg_logo.svg'
-import AnchorButton from './elements/AnchorButton'
-import MobileMenu from './MobileNav'
+import styled from "styled-components";
+import theme from "./theme";
+import Link from "next/link";
+import GDGLogo from "./icons/gdg_logo.svg";
+import AnchorButton from "./elements/AnchorButton";
+import MobileMenu from "./MobileNav";
 
-export const NAV_BREAKPOINT = '885px'
+export const NAV_BREAKPOINT = "885px";
 
 const StyledLink = styled.a`
   color: ${theme.colors.blue};
@@ -15,15 +15,15 @@ const StyledLink = styled.a`
   &:hover {
     color: ${theme.colors.blueLight};
   }
-`
+`;
 
 const Nav = styled.nav`
-  background: rgba(255,255,255,0.97);
+  background: rgba(255, 255, 255, 0.97);
   position: fixed;
   top: 0;
   width: 100%;
   z-index: 5;
-`
+`;
 
 const NavDesktopContainer = styled.div`
   display: none;
@@ -34,11 +34,11 @@ const NavDesktopContainer = styled.div`
     max-width: 980px;
     padding: 15px;
   }
-`
+`;
 
 const NavLinks = styled.div`
   margin-top: 9px;
-`
+`;
 
 const StaticLogo = () => (
   <Link href="/">
@@ -46,7 +46,7 @@ const StaticLogo = () => (
       <GDGLogo alt="GDG" width="70px" height="35px" />
     </a>
   </Link>
-)
+);
 
 export const Navbar = () => {
   return (
@@ -54,16 +54,28 @@ export const Navbar = () => {
       <NavDesktopContainer>
         <StaticLogo />
         <NavLinks>
-          <Link passHref href="/"><StyledLink>Home</StyledLink></Link>
-          <Link passHref href="/#speakers"><StyledLink>Speakers</StyledLink></Link>
-          <Link passHref href="/#schedule"><StyledLink>Talks</StyledLink></Link>
-          <Link passHref href="/#sponsors"><StyledLink>Sponsors</StyledLink></Link>
-          <Link passHref href="/location"><StyledLink>Location</StyledLink></Link>
-          <Link passHref href="/conduct"><StyledLink>Code of Conduct</StyledLink></Link>
+          <Link passHref href="/">
+            <StyledLink>Inicio</StyledLink>
+          </Link>
+          <Link passHref href="/#speakers">
+            <StyledLink>Ponentes</StyledLink>
+          </Link>
+          <Link passHref href="/#schedule">
+            <StyledLink>Charlas</StyledLink>
+          </Link>
+          <Link passHref href="/#sponsors">
+            <StyledLink>Patrocinadores</StyledLink>
+          </Link>
+          <Link passHref href="/location">
+            <StyledLink>Localización</StyledLink>
+          </Link>
+          <Link passHref href="/conduct">
+            <StyledLink>Código de conducta</StyledLink>
+          </Link>
           {/* <AnchorButton href="https://ti.to/gdg-new-orleans/devfest-new-orleans-2019" medium="true" target="_blank" rel="noopener noreferrer">Register now</AnchorButton> */}
         </NavLinks>
       </NavDesktopContainer>
       <MobileMenu />
     </Nav>
-  )
-}
+  );
+};
