01cc812545df79bd1284bd66059afa6740f9e26f
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-11-29 17:06:51 +0100

    Añadiendo sponsors

--- components/SponsorList.js
+++ components/SponsorList.js
@@ -1,43 +1,30 @@
-import {Fragment} from 'react'
+import { Fragment } from "react";
 import Sponsor from "./Sponsor";
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const List = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-evenly;
   flex-wrap: wrap;
-`
+`;
 
 const Heading = styled.h2`
   text-align: center;
-`
+`;
 
-const getSponsorList = (sponsors) => (
-  sponsors.map(sponsor => (
-    <Sponsor {...sponsor} key={sponsor.id} />
-  ))
-)
+const getSponsorList = (sponsors) =>
+  sponsors.map((sponsor) => <Sponsor {...sponsor} key={sponsor.id} />);
 
 const SponsorList = ({ sponsors }) => (
   <Fragment>
-    <Heading>Platinum</Heading>
-    <List>
-      {getSponsorList(sponsors.platinum)}
-    </List>
-    <Heading>Gold</Heading>
-    <List>
-      {getSponsorList(sponsors.gold)}
-    </List>
-    <Heading>Silver</Heading>
-    <List>
-      {getSponsorList(sponsors.silver)}
-    </List>
-    <Heading>Bronze</Heading>
-    <List>
-      {getSponsorList(sponsors.bronze)}
-    </List>
+    <Heading>Diamante 💎</Heading>
+    <List>{getSponsorList(sponsors.platinum)}</List>
+    <Heading>Oro </Heading>
+    <List>{getSponsorList(sponsors.gold)}</List>
+    <Heading>Plata</Heading>
+    <List>{getSponsorList(sponsors.silver)}</List>
   </Fragment>
-)
+);
 
-export default SponsorList
+export default SponsorList;
